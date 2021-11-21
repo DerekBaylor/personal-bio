8d5d8b8c02bf459574ea7527a260527b4f4a3f91
Derek Baylor <dbaylor25@gmail.com>
2021-11-20 19:52:33 -0600

    added bio

--- src/components/PersonalBio.js
+++ src/components/PersonalBio.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function PersonalBio() {
   return (
     <div>
-      <p>This is my bio.</p>
+      <p>My journey into tech started back in the early 90s when my father introduced me to Commander Keen on a 5.25in floppy disk, but as I've gotten older, I've found other uses for computers than just video games. My first jump into technology was when I built a database in Microsoft Access to track the maintenance performed on rental equipment when I worked for an event company. At the beginning of 2021, I decided to pursue a full-time career in web development and submitted my application to Nashville Software School, and started my yearlong boot camp in July.</p>
     </div>
   );
 }
