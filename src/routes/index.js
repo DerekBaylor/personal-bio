89e93fa31907ece4525ee1dbc1bd869c5d798023
Derek Baylor <dbaylor25@gmail.com>
2021-11-22 21:26:15 -0600

    created admin

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import ContactMe from '../views/ContactMe';
 import Home from '../views/Home';
 import LogIn from '../views/LogIn';
-// import LogIn from '../views/LogIn';
-// import ProjectForm from '../views/ProjectForm';
+import ProjectForm from '../views/ProjectForm';
 import Projects from '../views/Projects';
 
-export default function Routes() {
+export default function Routes({ admin }) {
   return (
     <>
       <Switch>
@@ -16,16 +15,20 @@ export default function Routes() {
         <Route exact path="/projects" component={Projects} />
         <Route exact path="/contactMe" component={ContactMe} />
         <Route exact path="/login" component={LogIn} />
-        {/* <Route exact path="/" component={() => <Home />} /> */}
-        {/* <Route exact path="/projects" component={() => <Projects />} /> */}
-        {/* <Route exact path="/contactMe" component={() => <ContactMe />} /> */}
-        {/* <Route exact path="/logIn" component={() => <LogIn />} /> */}
-        {/* <Route exact path="/projectForm" component={() => <ProjectForm />} /> */}
+        <Route
+          exact
+          path="/projectForm"
+          component={() => <ProjectForm admin={admin} />}
+        />
       </Switch>
     </>
   );
 }
 
-// Routes.propTypes = {
-//   uid: PropTypes.string.isRequired,
-// };
+Routes.propTypes = {
+  admin: PropTypes.shape,
+};
+
+Routes.defaultProps = {
+  admin: null,
+};
--- src/views/ProjectForm.js
+++ src/views/ProjectForm.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function ProjectForm() {
-  return <h1>This is ProjectForm</h1>;
+export default function ProjectForm({ admin }) {
+  console.warn('Project Form Console Warn', admin);
+  return <h1>This is the ProjectForm</h1>;
 }
+
+ProjectForm.propTypes = {
+  admin: PropTypes.shape.isRequired,
+};
